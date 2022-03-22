6a96613c774c83229846da5c7f57c1c58dad2c4a
Andrei Teodoru <andreiteodoru@aol.com>
2022-03-22 09:17:20 +0200

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
+//use std::fs::File;
+//use std::process::Command;
 use std::{fs, env};
-use std::os::unix::fs::MetadataExt;
-use std::os::unix::io::AsRawFd;
-use std::io::Read;
+//use std::os::unix::fs::MetadataExt;
+//use std::os::unix::io::AsRawFd;
+//use std::io::Read;
+
+fn print (file:&String){
+    println!("In file {}", file);
+
+    let contents = fs::read_to_string(file)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
+fn size (file:&String)->u64{
+    let size = std::fs::metadata(file).unwrap().len();
+    size
+}
+
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    //let folder = &args[1];ex1
 
-    let folder = &args[1];
+    let file = &args[1];
+    let command= &args[2];
     
-    let paths = fs::read_dir(folder).unwrap();
+
+    
+    
+    /*let paths = fs::read_dir(folder).unwrap();
 
     for path in paths {
         println!("Name: {}", path.unwrap().path().display())
+    }*/
+    //ex1
+
+    
+
+    match command.as_str(){
+        "print"=>print(file),
+        "size"=>println!("Marimea fisierului este: {} bytes",size(file)),
+        _=> std::process::exit(-1)
     }
+
+   
 }
