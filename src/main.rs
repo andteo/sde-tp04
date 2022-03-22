c4a536a39ec03b57df15b6ae36f731602e8edbad
Andrei Teodoru <andreiteodoru@aol.com>
2022-03-22 08:29:04 +0200

    Primul exercitiu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{fs, env};
+use std::os::unix::fs::MetadataExt;
+use std::os::unix::io::AsRawFd;
+use std::io::Read;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let folder = &args[1];
+    
+    let paths = fs::read_dir(folder).unwrap();
+
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
 }
